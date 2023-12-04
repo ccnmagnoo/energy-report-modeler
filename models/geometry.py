0efe733d6ab271ac98b288aca9ab68b197de924d
ccnmagnoo <crcamposn@gmail.com>
2023-12-04 08:48:18 -0300

    omega calculus

--- models/geometry.py
+++ models/geometry.py
@@ -8,5 +8,11 @@ class GeoPosition:
         
 class Orientation:
     def __init__(self,inclination:float = 33.0,azimuth:float = 0) -> None:
-        self.inclination = inclination,
-        self.azimuth = azimuth;
+        self.inclination = inclination
+        self.azimuth = azimuth
+    
+    def angleDifference(self, inclination:float,azimuth:float)->float:
+        azimuthDelta:float = self.azimuth - azimuth
+        inclinationDelta:float = self.inclination - inclination
+        return (azimuthDelta**2 + inclinationDelta**2)**(1/2)
+        
