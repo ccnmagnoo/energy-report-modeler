50c16a8d0bc1c1df18fd41d5dea7b5c5c862e28a
ccnmagnoo <crcamposn@gmail.com>
2023-12-04 08:48:02 -0300

    types output

--- models/econometrics.py
+++ models/econometrics.py
@@ -6,16 +6,16 @@ class Currency(Enum):
     #cspell:disable
     CLP ='peso chileno'
     USD ='dolar'
-    EUR = 'euro'
+    EUR ='euro'
 
 class Cost:
     IVA = 0.19
     def __init__(self, bruteCost:float = 0,currency:Currency = Currency.CLP) -> None:
         self.bruteCost:float = bruteCost
-        self.currency = currency
+        self.currency:Currency = currency
     
-    def tax(self):
+    def tax(self)->float:
         return self.bruteCost*self.IVA    
         
-    def netCost(self):
+    def netCost(self)->float:
         return self.tax() + self.bruteCost
