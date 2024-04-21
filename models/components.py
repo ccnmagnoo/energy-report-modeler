5d5af03680817e7a5fe75e3d131785217556ffca
ccnmagnoo <crcamposn@gmail.com>
2024-04-21 14:15:38 -0400

    Assembly Class component wrapper

--- models/components.py
+++ models/components.py
@@ -1,8 +1,11 @@
 """data"""
 from enum import Enum
 
-from models.econometrics import Cost, Currency
 
+if __name__ == "__main__":
+    from econometrics import Cost,Currency
+else:
+    from models.econometrics import Cost, Currency
 
 class Tech(Enum):
     """in this project will only use PV, but in the future will be expanded to other techs"""
@@ -35,3 +38,30 @@ class Component:
     def total_cost_after_tax(self,currency:Currency|None):
         """cost after taxes"""
         return self.quantity*self.cost.cost_after_tax(currency)[0],currency or self.cost.currency
+
+type Package = dict[str,list[Component]]
+class Assembly:
+    """list of components"""
+    package:Package = {}
+
+    def __init__(self,group:str,*comp:Component) -> None:
+        if group not in self.package:
+            ##init
+            self.package[group] = []
+        self.package[group]:list[Component] = [*self.package[group],*comp] # type: ignore
+
+    def __getitem__(self,group:str)->list[Component]:
+        if group not in self.package:
+            raise ValueError('no group on list ->',list(self.package.keys()))
+        return self.package[group]
+
+
+#manual test
+if __name__ == "__main__":
+    com1 = Component('test')
+    com2 = Component('test')
+    com3 = Component('test')
+    ass = Assembly("energético",com1,com2,com3)
+    
+
+    print(ass["energétic"])
