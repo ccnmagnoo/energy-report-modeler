5197a3b58bab9f313a2eb1bc4d817089d5bd0f58
ccnmagnoo <crcamposn@gmail.com>
2024-07-10 16:16:35 -0400

    second param always true

--- models/photovoltaic.py
+++ models/photovoltaic.py
@@ -158,7 +158,7 @@ class Photovoltaic(Component):
         ) -> None:
         #auxiliary values
         aux_cost:Cost
-        if (cost is None) and cost_model:
+        if cost is None:
             aux_cost = Cost(cost_model(technical_sheet.power)*technical_sheet.power,currency=Currency.CLP)
         else:
             aux_cost = cost
