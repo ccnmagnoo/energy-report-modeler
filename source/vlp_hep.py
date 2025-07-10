ae2a71492f0fc8895908195ff26477d94f001444
ccnmagnoo <crcamposn@gmail.com>
2025-07-09 20:48:35 -0400

    hep project

---
+++ source/vlp_hep.py
@@ -0,0 +1,138 @@
+"""permanent project data"""
+#cspell: disable
+from typing import Literal
+from models.components import Component as Equip, Specs, Tech
+from models.consumption import ElectricityBill as Bill
+from models.econometrics import Cost, Currency as Curr
+from models.geometry import Orientation
+from models.inventory import Building
+from models.photovoltaic import PvInput
+from libs.repository import repoEquipment,panelRepo
+
+type Subject = Literal['project','consumptions','components']
+
+#gen
+PANELS:int=3*21+2*4+2*21
+POWER:int=PANELS*655/1000
+#
+
+data:dict[Subject,any] = {
+    'project':{
+        'title':'FV H. E.Pereira R.',
+        'connection_type':'netbilling',
+        'technology':[Tech.PHOTOVOLTAIC],
+        'building':Building(
+            geolocation=(-33.056764, -71.589541),
+            name='H. Dr. Eduardo Pereira',
+            address='Ibsen S/N',
+            city='Valparaíso'),
+    },
+    'consumptions':{
+        'cost_increment':5.8,
+        'client_id':'672250-4',
+        'measurer_id':'10034493',
+        'contract_id':'AT-4.3 ',
+        'consumption':[
+            Bill("13-11-2023","12-12-2023",15_720,1_495_284,Curr.CLP,('AT','_43'),p_leida=53.3,dmax=80.7),#costo_dmax=1_601_133),
+            Bill("12-12-2023","11-01-2024",15_660,1_487_594,Curr.CLP,('AT','_43'),p_leida=53.5,dmax=80.7),#costo_dmax=1.601_133),
+            Bill("11-01-2024","09-02-2024",16_560,1_573_087,Curr.CLP,('AT','_43'),p_leida=59.2,dmax=80.7),#costo_dmax=1_601_133),
+            Bill("09-02-2024","11-03-2024",15_900,1_510_934,Curr.CLP,('AT','_43'),p_leida=51.7,dmax=80.7),#costo_dmax=1_601_133),
+            Bill("11-03-2024","11-04-2024",16_680,1_584_487,Curr.CLP,('AT','_43'),p_leida=53.0,dmax=80.7),#costo_dmax=1_601_133),
+            Bill("11-04-2024","13-05-2024",17_880,1_698_480,Curr.CLP,('AT','_43'),p_leida=66.8,dmax=80.7),#costo_dmax=1_601_133),
+            Bill("13-05-2024","11-06-2024",17_580,1_761_547,Curr.CLP,('AT','_43'),p_leida=67.6,dmax=80.7),#costo_dmax=1_779_615),
+            Bill("11-06-2024","11-07-2024",19_020,2_423_419,Curr.CLP,('AT','_43'),p_leida=81.1,dmax=81.1),#costo_dmax=1_812_578),
+            Bill("11-07-2024","11-08-2024",20_460,2_606_897,Curr.CLP,('AT','_43'),p_leida=82.0,dmax=82.0),#costo_dmax=1_823_287),
+            Bill("04-08-2024","03-09-2024",19_980,2_163_495,Curr.CLP,('AT','_43'),p_leida=72.6,dmax=81.6),#costo_dmax=1_819_721),
+            Bill("03-09-2024","09-10-2024",16_080,2_188_191,Curr.CLP,('AT','_43'),p_leida=59.2,dmax=81.6),#costo_dmax=1_926_901),
+            Bill("09-10-2024","11-11-2024",17_940,2_411_302,Curr.CLP,('AT','_43'),p_leida=55.9,dmax=81.6),#costo_dmax=1_925_662),
+        ]
+    },
+    'components':{
+        'generator':(
+            panelRepo['CS 655W'],# equipment
+            PvInput(
+                description='FV 655W mod01',
+                quantity=3*21,
+                orientation=Orientation(5,20),
+                ),
+            PvInput(
+                description='FV 655W mod02',
+                quantity=4*2,
+                orientation=Orientation(5,20),
+                ),
+            PvInput(
+                description='FV 655W mod03',
+                quantity=2*21,
+                orientation=Orientation(15,35),
+                ),
+        ),
+        'install':(
+            'instalación',
+            repoEquipment['Inverter']['CS 50kW H'],# inverter
+            repoEquipment['Medidor']['FR 3F'],# lectura
+            Equip(
+                description='eléctrica interior',
+                specification=Specs(
+                    category='Obra',
+                    brand='conexionado',
+                    model='interiores'
+                    ),
+                cost_per_unit=Cost(225_000),
+                quantity=POWER#power kW
+            ),
+            Equip(
+                description='estructura montaje',
+                specification=Specs(
+                    category='Montaje',
+                    montaje='coplanar',
+                    ),
+                cost_per_unit=Cost(45_000),
+                quantity=PANELS#panels
+                ),
+            ),
+        'storage':(#0 hours for not install storage at all
+            'almacenamiento',0,'24/5',
+            repoEquipment['Monitor']['VC 700'],
+            repoEquipment['Regulator']['VT 45A']
+        ),
+
+        'accesories':(
+            'obras',
+            Equip(
+                description='Faenas',
+                specification=Specs(
+                    category='Obra',
+                    brand='equipamiento',
+                    model='provisorio'),
+                cost_per_unit=Cost(18_750),
+                quantity=PANELS#panels
+                ),
+            Equip(
+                description='Capacitación',
+                specification=Specs(
+                    category='Obra',
+                    brand='Uso y',
+                    model='mantenimiento',
+                    taller='3 Hrs',
+                    manual='6 u. impreso',
+                    ),
+                cost_per_unit=Cost(250_000),
+                ),
+            Equip(
+                description='Letrero',
+                specification=Specs(
+                    category='Obra',
+                    brand='pliego',
+                    model='estructura',
+                    specs_url='#pliegos-técnicos',
+                    dim='3.6m x 1.5m',
+                    ),
+                cost_per_unit=Cost(450_000),
+                )
+        ),
+        'overloads':{
+            'gastos_gral':25,
+            'utilidad':20
+        }
+    }
+}
